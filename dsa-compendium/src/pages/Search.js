270338cbb073dbb8c1d0faf647b6b8d8fcd4edff
sazfar26 <sufianazfar@gmail.com>
2025-01-18 20:11:43 -0500

    Completed Search Bar
    The text above the search bar is correctly animated, and the placeholder shuffles through suggestions. Need to revise the shuffling so that it constantly shuffles and doesn't stop after getting to the length of the array

--- dsa-compendium/src/pages/Search.js
+++ dsa-compendium/src/pages/Search.js
@@ -6,16 +6,35 @@ import "./Search.css";
 
 function Search() {
 
+    //this will render the the text above the search bar, as well as pick a random suggestion topic from the array
+    let searchBar = document.querySelector(".searchbar");
     const renderText = (element) => {
         let searchTitleDiv = document.getElementById("searchTitle");
-        let searchTitleText = searchTitleDiv.innerText;
-        searchTitleDiv.innerText = "";
+        let searchTitleText = searchTitleDiv.textContent;
+        searchTitleDiv.textContent = "";
+
+        //generates the text above the search bar
         for (let i = 0; i < searchTitleText.length; i++) {
             setTimeout(() => {
-                searchTitleDiv.innerText += searchTitleText.charAt(i);
-            }, 50);
+                searchTitleDiv.textContent += searchTitleText.charAt(i);
+            }, 45 * i);
+        }
+
+        //TODO: Add actual list of topics, based on this semester's 2214 curriculum
+        //also, try to revise this so it keeps cycling through the topics even after picking each one once
+        const topics = ["Arrays", "Linked Lists", "Stacks", "Queues", "Trees", "Graphs", "Hash Tables", "Sorting", "Searching"];
+        let searchBar = document.querySelector(".searchbar");
+        let randomIndex = Math.floor(Math.random() * topics.length);
+        searchBar.placeholder = topics[randomIndex];
+        for (let j = 0; j < topics.length; j++) {
+            let randomIndex = Math.floor(Math.random() * topics.length);
+            setTimeout(() => {
+
+                searchBar.placeholder = topics[randomIndex];
+            }, 2600 * j);
         }
     }
+
 
     useEffect(() => {
         document.title= "Search";
@@ -24,8 +43,8 @@ function Search() {
 
     return (
         <div className="search">
-        <div id="searchTitle"><h1>What topic do you <br /> need help with today?</h1></div>
-        <input type="text" className="searchbar"></input>
+        <div id="searchTitle"><h1>What topic do you <br /> need help with?</h1></div>
+        <input type="text" className="searchbar" placeholder="Stacks"></input>
         <div style={{height: "800px"}}></div>
         </div>
     )
